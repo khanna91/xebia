f6f712bd5f0c344643ce5cd138a12e980874a9e8
Rahul Khanna <rahul.khanna@ofbusiness.com>
2017-01-15 05:07:27 +0530

    code clean up

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('app', [
-        'ui.router', 'ngMessages', 'ngSanitize', 'ngAnimate', 'LocalStorageModule',
+        'ui.router', 'ngSanitize', 'LocalStorageModule',
         // custom modules
         'utils', 'product'
     ]);
