8c678886626caf100c90c1190218df4d767486cd
Rahul Khanna <rahul.khanna@ofbusiness.com>
2017-01-14 16:14:46 +0530

    removing keys

--- gulpfile.js
+++ gulpfile.js
@@ -12,25 +12,16 @@ gulp.paths = {
 require('require-dir')('./gulp');
 
 gulp.task('buildlocal', ['clean'], function () {
-    gulp.paths.googleMapKey = 'AIzaSyCL-mduxahO1neIBX2HYWduSitYKgkULUk';
-    gulp.paths.mapApiKey = 'AIzaSyBmjWgtryGkDDWoVgGIHJ5MM_QZDLCJ9kI';
-    gulp.paths.zomatoApiKey = '00c4ef8fd362359559f9322de537fb35';
     gulp.env = 'local';
     gulp.start('buildapp-local');
 });
 
 gulp.task('builddev', ['clean'], function () {
-    gulp.paths.googleMapKey = 'AIzaSyCL-mduxahO1neIBX2HYWduSitYKgkULUk';
-    gulp.paths.mapApiKey = 'AIzaSyBmjWgtryGkDDWoVgGIHJ5MM_QZDLCJ9kI';
-    gulp.paths.zomatoApiKey = '00c4ef8fd362359559f9322de537fb35';
     gulp.env = 'staging';
     gulp.start('buildapp');
 });
 
 gulp.task('buildprod', ['clean'], function () {
-    gulp.paths.googleMapKey = 'AIzaSyCL-mduxahO1neIBX2HYWduSitYKgkULUk';
-    gulp.paths.mapApiKey = 'AIzaSyBmjWgtryGkDDWoVgGIHJ5MM_QZDLCJ9kI';
-    gulp.paths.zomatoApiKey = '00c4ef8fd362359559f9322de537fb35';
     gulp.env = 'production';
     gulp.start('buildapp');
 });
